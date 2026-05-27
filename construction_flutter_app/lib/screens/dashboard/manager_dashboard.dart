390dad89735603cb90aa6d4b0de991922c24733f
Sukhshum2628 <2022a1r002@mietjammu.in>
2026-05-27 11:56:39 +0530

    fix(dashboard): import user_model to resolve UserRole compiler error

--- construction_flutter_app/lib/screens/dashboard/manager_dashboard.dart
+++ construction_flutter_app/lib/screens/dashboard/manager_dashboard.dart
@@ -9,6 +9,7 @@ import 'package:construction_app/providers/deviation_provider.dart';
 import 'package:construction_app/providers/auth_provider.dart';
 import 'package:construction_app/providers/resource_log_provider.dart';
 import '../../models/project_model.dart';
+import '../../models/user_model.dart';
 import '../../utils/design_tokens.dart';
 import 'package:construction_app/providers/weather_provider.dart';
 import 'package:construction_app/providers/user_provider.dart';
